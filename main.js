801f389acb0445a6f9a8b4f7b0e8a2adca5aa965
maria <mpramos78915400@gmail.com>
2026-04-20 22:26:19 -0400

    buscador

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
+import { Buscador } from "./componentes/Buscador.js";
 import { Card } from "./componentes/Card.js";
 import { productos } from "./data/productos.js";
 
 const app = document.getElementById('app')
-const renderProductos = ()=>{
-    app.innerHTML = productos.map((producto)=>{return Card(producto)}).join('')
+
+app.classList.add('text-right')
+
+const renderLayout = () =>{
+    app.innerHTML =`
+    ${Buscador()}
+    <div  class="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 px-4" id ="contenedorProductos"></div>
+    `
 }
-renderProductos()
+
+const renderProductos = (lista)=>{
+    const contenedor = document.getElementById('contenedorProductos')
+    contenedor.innerHTML = lista.map((producto)=>{return Card(producto)}).join('')
+}
+
+const activarBuscador = ()=>{
+    const input = document.getElementById('inputBusqueda')
+    input.addEventListener('input', (e)=>{
+        const texto = e.target.value.toLowerCase()
+        const filtrados = productos.filter(p=>p.title.toLocaleLowerCase().includes(texto))
+        renderProductos(filtrados)
+    })
+}
+
+renderLayout()
+renderProductos(productos)
+activarBuscador()

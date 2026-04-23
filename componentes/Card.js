358e3bf003578e2c680519b1309653f9bb5ab41c
maria <mpramos78915400@gmail.com>
2026-04-22 22:36:47 -0400

    carrito

--- componentes/Card.js
+++ componentes/Card.js
@@ -7,6 +7,12 @@ export const Card = (producto) =>{
             <h2 class = "text-lg font-semibold mb-2 line-clamp-2">${producto.title}</h2>
             <p class = "text-gray-500 line-clamp-2">${producto.description}</p>
             <span class = "text-blue-600 font-bold text-lg line-clamp-2">${producto.price}</span>
-        </div> 
+            <button 
+            class = "btn-agregar w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+            data-id="${producto.id}"
+            >
+            Agregar al carrito
+            </button>
+            </div> 
     `
 }
